3f3bef2e0220de3803df164675e4b8e17df2e992
rsaavedravaldivia <rsaavedravaldivia@hccfl.edu>
2024-04-21 16:57:49 -0400

    program6

---
+++ SaavedraProgram6/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Location = new Point(311, 352);
+            button2.Margin = new Padding(2, 1, 2, 1);
+            button2.Name = "button2";
+            button2.Size = new Size(92, 29);
+            button2.TabIndex = 24;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(500, 292);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 20);
+            label6.TabIndex = 23;
+            label6.Text = "Result";
+           
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(487, 291);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 20);
+            label7.TabIndex = 22;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(345, 291);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 20);
+            label5.TabIndex = 21;
+            label5.Text = "Average is:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(92, 286);
+            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(164, 29);
+            button1.TabIndex = 20;
+            button1.Text = "Calculate Average";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(244, 220);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(104, 20);
+            label4.TabIndex = 19;
+            label4.Text = "Third Number:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(244, 166);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 20);
+            label3.TabIndex = 18;
+            label3.Text = "Second Number:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(244, 111);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 20);
+            label2.TabIndex = 17;
+            label2.Text = "First Number:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(244, 19);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.Yes;
+            label1.Size = new Size(219, 20);
+            label1.TabIndex = 16;
+            label1.Text = "Program #6:  Ronaldo Saavedra";
+          
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(380, 214);
+            textBox3.Margin = new Padding(2, 1, 2, 1);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(101, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(378, 162);
+            textBox2.Margin = new Padding(2, 1, 2, 1);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(101, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(378, 109);
+            textBox1.Margin = new Padding(2, 1, 2, 1);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(101, 27);
+            textBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(668, 396);
+            Controls.Add(button2);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Margin = new Padding(2, 1, 2, 1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Label label6;
+        private Label label7;
+        private Label label5;
+        private Button button1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ SaavedraProgram6/Form1.cs
@@ -0,0 +1,44 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private double average = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                {
+                    average = (double.Parse(textBox1.Text) + double.Parse(textBox2.Text) + double.Parse(textBox3.Text)) / 3;
+                    label6.Text = average.ToString();
+                }
+                else { return; }
+            }
+            catch (Exception)
+            {
+
+                label6.Text = "Invalid Input.";
+            }
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
